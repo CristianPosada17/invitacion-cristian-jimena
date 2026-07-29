059a35a9ae7611b649a86e9b58540712bdd729a1
Cristian Posada <cristianadan0@gmail.com>
2026-07-29 16:29:26 -0600

    Add animation

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker · Boda Cristian & Jimena (PWA)
-const CACHE = "bodacj-v18";
+const CACHE = "bodacj-v19";
 const ASSETS = ["/", "/logo-boda.png", "/og-boda.png", "/paper-texture.jpg", "/Sobre.mp4", "/sobre-poster.png", "/galeria/foto-8.jpg", "/manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
