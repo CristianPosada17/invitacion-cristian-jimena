f12ca80ae177606f148194f2c9dca8de4f92a945
Cristian Posada <cristianadan0@gmail.com>
2026-07-29 13:51:59 -0600

    Move component Regalos & fix Isues Album

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker · Boda Cristian & Jimena (PWA)
-const CACHE = "bodacj-v6";
+const CACHE = "bodacj-v7";
 const ASSETS = ["/", "/logo-boda.png", "/og-boda.png", "/Sobre.mp4", "/sobre-poster.png", "/galeria/foto-8.jpg", "/manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
